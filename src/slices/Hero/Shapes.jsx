5c0ad787fedbe3c00a057b965aa9a79ae33c71d6
amine alaoui <omari1amineco@gmail.com>
2024-02-15 11:33:31 +0100

    feat: import ContactShadows from react three fiber

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -12,7 +12,14 @@ export function Shapes() {
         dpr={[1, 1.5]}
         camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}
       >
-        <Suspense fallback={null}></Suspense>
+        <Suspense fallback={null}>
+          <ContactShadows
+            position={[0, -3.5, 0]}
+            opacity={0.65}
+            scale={40}
+            blur={1}
+          ></ContactShadows>
+        </Suspense>
       </Canvas>
     </div>
   );
