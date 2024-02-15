092199a908c84ce60944207e840b221f08fb838f
amine alaoui <omari1amineco@gmail.com>
2024-02-15 11:26:20 +0100

    feat: import Canvas comp from react three fiber to Shapes component

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -1,9 +1,10 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
 
 export function Shapes() {
   return (
     <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
-      Shapes
+      <Canvas></Canvas>
     </div>
   );
 }
