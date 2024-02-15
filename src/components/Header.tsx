f219ec3284fde777fc9aa4050bcb12c76c5cb1e8
amine alaoui <omari1amineco@gmail.com>
2024-02-15 17:35:14 +0100

    feat: add a link item to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
 import { createClient } from "@/prismicio";
+import Link from "next/link";
 import React from "react";
 
 export default async function Header() {
   const client = createClient();
   const settings = await client.getSingle("settings");
 
-  return <header>Header</header>;
+  return (
+    <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/" aria-label="Home page">
+              {settings.data.name}
+            </Link>
+          </li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
