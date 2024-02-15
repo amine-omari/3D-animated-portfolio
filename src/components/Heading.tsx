4262ffd13a36c86ff2021e047b59cc45da334149
amine alaoui <omari1amineco@gmail.com>
2024-02-15 20:26:50 +0100

    feat: add props type in Heading component

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,10 @@
+type HeadingProps = {
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  size?: "xl" | "lg" | "md" | "sm";
+  children: React.ReactNode;
+  className?: string;
+};
+
 export default function Heading() {
   return (
     <Comp>
