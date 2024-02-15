4bacb7937fc256befe07fc422dc08595af8b08e0
amine alaoui <omari1amineco@gmail.com>
2024-02-15 17:13:02 +0100

    feat: add an await function to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 export default async function Header() {
   const client = createClient();
+  const settings = await client.getSingle("settings");
 
   return <header>Header</header>;
 }
