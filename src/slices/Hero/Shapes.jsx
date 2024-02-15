24eec35009ff3ec4719e2d943e1c9f1be994516a
amine alaoui <omari1amineco@gmail.com>
2024-02-15 11:25:42 +0100

    feat: add style to the container

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Shapes = () => {
-  return <div>Shapes</div>;
-};
-
-export default Shapes;
+export function Shapes() {
+  return (
+    <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
+      Shapes
+    </div>
+  );
+}
