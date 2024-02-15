337c596f1e6a68a2d867d548d04228e6fdcaecdc
amine alaoui <omari1amineco@gmail.com>
2024-02-15 20:30:32 +0100

    feat: import Heading component to Biography page

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,4 +1,5 @@
 import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
@@ -16,7 +17,11 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]"></div>
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+        <Heading as="h1" size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </Heading>
+      </div>
     </Bounded>
   );
 };
