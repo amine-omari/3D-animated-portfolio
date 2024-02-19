51c736a20d0779e2b5b5a6aaa044edfa4002f26b
amine alaoui <omari1amineco@gmail.com>
2024-02-19 12:34:50 +0100

    feat: import MdArrowoutWard icon from react icon to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,7 @@ import { KeyTextField, LinkField } from "@prismicio/client";
 import { PrismicNextLink } from "@prismicio/next";
 import clsx from "clsx";
 import React from "react";
+import { MdArrowOutward } from "react-icons/md";
 
 type ButtonProps = {
   linkField: LinkField;
@@ -26,7 +27,7 @@ export default function Button({
     >
       <span className="absolute inset-0 z-0 h-full translate-y-9 bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0"></span>
       <span className="relative flex items-center justify-center gap-2">
-        {label} {showIcon && "=>"}
+        {label} {showIcon && <MdArrowOutward className="inline-block" />}
       </span>
     </PrismicNextLink>
   );
