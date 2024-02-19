b35c83eaf827716d988b2e0577700c447a2528d4
amine alaoui <omari1amineco@gmail.com>
2024-02-19 13:06:23 +0100

    fix: style

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
+import clsx from "clsx";
 import React, { useRef } from "react";
 
 type AvatarProps = {
@@ -11,12 +14,14 @@ export default function Avatar({ image, className }: AvatarProps) {
   const component = useRef(null);
 
   return (
-    <div ref={component}>
-      <div>
+    <div ref={component} className={clsx("relative h-full w-full", className)}>
+      <div className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0f">
         <PrismicNextImage
           field={image}
-          className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"
+          className="avatar-image h-full w-full object-fill"
+          imgixParams={{ q: 90 }}
         />
+        <div className="highlight absolute inset-0 hiddenf w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0f md:block"></div>
       </div>
     </div>
   );
