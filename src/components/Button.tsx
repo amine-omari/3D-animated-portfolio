92f21477ba5dbcfbb8b14f6bd64e1d6c6ebd83e4
amine alaoui <omari1amineco@gmail.com>
2024-02-19 11:09:40 +0100

    feat: add props type to Buton component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { KeyTextField, LinkField } from "@prismicio/client";
+import React from "react";
 
-export default function Button() {
-  return (
-    <div>Button</div>
-  )
+type ButtonProps = {
+  linkField: LinkField;
+  label: KeyTextField;
+  showcon?: boolean;
+  className: string;
+};
+
+export default function Button({
+  linkField,
+  label,
+  showcon,
+  className,
+}: ButtonProps) {
+  return <div>Button</div>;
 }
