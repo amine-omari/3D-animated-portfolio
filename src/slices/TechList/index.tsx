2143adad233db8b76852cbce9716c2007b69973f
amine alaoui <omari1amineco@gmail.com>
2024-02-20 14:13:01 +0100

    fix: TechList component style

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -36,7 +36,7 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
               >
                 {tech_name}
               </span>
-              <span>
+              <span className="text-3xl">
                 <MdCircle />
               </span>
             </React.Fragment>
