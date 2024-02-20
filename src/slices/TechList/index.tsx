d63f86a757723e66022a2a202c507878e0553485
amine alaoui <omari1amineco@gmail.com>
2024-02-20 14:11:14 +0100

    feat: add a map function to TechList component

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,6 +1,7 @@
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import React from "react";
 
 /**
  * Props for `TechList`.
@@ -16,7 +17,28 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <Heading size="lg" as="h2">{slice.primary.heading}</Heading>
+      <Heading size="lg" as="h2">
+        {slice.primary.heading}
+      </Heading>
+      {slice.items.map(({ tech_color, tech_name }, index) => (
+        <div
+          key={index}
+          className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+        >
+          {Array.from({ length: 15 }, (_, index) => (
+            <React.Fragment key={index}>
+              <span
+                className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                style={{
+                  color: index === 7 && tech_color ? tech_color : "inherit",
+                }}
+              >
+                {tech_name}
+              </span>
+            </React.Fragment>
+          ))}
+        </div>
+      ))}
     </section>
   );
 };
