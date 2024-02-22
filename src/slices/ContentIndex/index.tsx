c8412d5b9773a2270445332361091b49a8519478
amine alaoui <omari1amineco@gmail.com>
2024-02-22 13:30:27 +0100

    fgeat: add style to rich text container

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -21,7 +21,7 @@ const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
         {slice.primary.heading}
       </Heading>
       {isFilled.richText(slice.primary.description) && (
-        <div>
+        <div className="prose prose-xl prose-invert mb-10">
           <PrismicRichText field={slice.primary.description} />
         </div>
       )}
