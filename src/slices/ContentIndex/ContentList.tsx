7ae72cdedeb9fec45070c283b8ae76d13da48950
amine alaoui <omari1amineco@gmail.com>
2024-02-23 12:59:39 +0100

    feat: imoprt isFilled to ContentList component

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,4 +1,4 @@
-import { Content } from "@prismicio/client";
+import { Content, isFilled } from "@prismicio/client";
 import Link from "next/link";
 import React from "react";
 import { MdArrowOutward } from "react-icons/md";
@@ -23,25 +23,29 @@ export default function ContentList({
       <ul className="grid border-b border-b-slate-100">
         {items.map((item, index) => (
           <>
-            <li key={index} className="list-item opacity-0f">
-              <Link
-                href={urlPrefixes + "/" + item.uid}
-                className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-                aria-label={item.data.title}
-              >
-                <div className="flex flex-col">
-                  <span className="text-3xl font-bold">{item.data.title}</span>
-                  <div className="flex gap-3 text-yellow-400 text-lg font-bold">
-                    {item.tags.map((tag, index) => (
-                      <span key={index}>{tag}</span>
-                    ))}
+            {isFilled.keyText(item.data.title) && (
+              <li key={index} className="list-item opacity-0f">
+                <Link
+                  href={urlPrefixes + "/" + item.uid}
+                  className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+                  aria-label={item.data.title}
+                >
+                  <div className="flex flex-col">
+                    <span className="text-3xl font-bold">
+                      {item.data.title}
+                    </span>
+                    <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                      {item.tags.map((tag, index) => (
+                        <span key={index}>{tag}</span>
+                      ))}
+                    </div>
                   </div>
-                </div>
-                <span className="ml-auto flex items-center gap-2 font-medium text-xl md:ml-0">
-                  {viewMoreText} <MdArrowOutward />
-                </span>
-              </Link>
-            </li>
+                  <span className="ml-auto flex items-center gap-2 font-medium text-xl md:ml-0">
+                    {viewMoreText} <MdArrowOutward />
+                  </span>
+                </Link>
+              </li>
+            )}
           </>
         ))}
       </ul>
