3cc41f5093f4cb3f91f5e0c117c42693fcafb7bd
amine alaoui <omari1amineco@gmail.com>
2024-02-23 12:58:30 +0100

    feat: add aria-label to next link

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -22,24 +22,27 @@ export default function ContentList({
     <div>
       <ul className="grid border-b border-b-slate-100">
         {items.map((item, index) => (
-          <li key={index} className="list-item opacity-0f">
-            <Link
-              href={urlPrefixes + "/" + item.uid}
-              className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-            >
-              <div className="flex flex-col">
-                <span className="text-3xl font-bold">{item.data.title}</span>
-                <div className="flex gap-3 text-yellow-400 text-lg font-bold">
-                  {item.tags.map((tag, index) => (
-                    <span key={index}>{tag}</span>
-                  ))}
+          <>
+            <li key={index} className="list-item opacity-0f">
+              <Link
+                href={urlPrefixes + "/" + item.uid}
+                className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+                aria-label={item.data.title}
+              >
+                <div className="flex flex-col">
+                  <span className="text-3xl font-bold">{item.data.title}</span>
+                  <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                    {item.tags.map((tag, index) => (
+                      <span key={index}>{tag}</span>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <span className="ml-auto flex items-center gap-2 font-medium text-xl md:ml-0">
-                {viewMoreText} <MdArrowOutward />
-              </span>
-            </Link>
-          </li>
+                <span className="ml-auto flex items-center gap-2 font-medium text-xl md:ml-0">
+                  {viewMoreText} <MdArrowOutward />
+                </span>
+              </Link>
+            </li>
+          </>
         ))}
       </ul>
     </div>
