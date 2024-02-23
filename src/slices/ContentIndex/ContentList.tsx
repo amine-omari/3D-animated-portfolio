d25150b43d5c4ef1192794fdab4e09bd1081d4e0
amine alaoui <omari1amineco@gmail.com>
2024-02-23 12:30:11 +0100

    feat: add style to li elements

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -18,8 +18,11 @@ export default function ContentList({
     <div>
       <ul className="grid border-b border-b-slate-100">
         {items.map((item, index) => (
-          <li key={index}>
-            <a href="">
+          <li key={index} className="list-item opacity-0f">
+            <a
+              href=""
+              className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+            >
               <div>
                 <span>{item.data.title}</span>
                 <div>
