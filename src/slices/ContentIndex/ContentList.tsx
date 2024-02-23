5ba1d490a1a12a993de4b698e3efdc5805689c99
amine alaoui <omari1amineco@gmail.com>
2024-02-23 12:07:49 +0100

    feat: add ContentListProps props type to ContentaList component

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -3,6 +3,9 @@ import React from "react";
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+  ContentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"]
 };
 
 export default function ContentList() {
