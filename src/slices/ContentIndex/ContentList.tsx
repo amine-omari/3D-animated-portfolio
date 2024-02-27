3747b1a20cc6ada450bf15ea34cd98420ac4af6f
amine alaoui <omari1amineco@gmail.com>
2024-02-27 11:49:32 +0100

    fix: ContentList component

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -61,7 +61,14 @@ export default function ContentList({
     };
   }, [currentItem]);
 
-  const contentImages = items.map((item) => {
+  const contentImages = [
+    ...items,
+    ...items,
+    ...items,
+    ...items,
+    ...items,
+    ...items,
+  ].map((item) => {
     const image = isFilled.image(item.data.hover_image)
       ? item.data.hover_image
       : fallbackItemImage;
@@ -88,37 +95,39 @@ export default function ContentList({
         className="grid border-b border-b-slate-100"
         onMouseLeave={onMouseLeave}
       >
-        {items.map((item, index) => (
-          <>
-            {isFilled.keyText(item.data.title) && (
-              <li
-                key={index}
-                onMouseEnter={() => onMouseEnter(index)}
-                className="list-item opacity-0f"
-              >
-                <Link
-                  href={urlPrefixes + "/" + item.uid}
-                  className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-                  aria-label={item.data.title}
+        {[...items, ...items, ...items, ...items, ...items, ...items].map(
+          (item, index) => (
+            <>
+              {isFilled.keyText(item.data.title) && (
+                <li
+                  key={index}
+                  onMouseEnter={() => onMouseEnter(index)}
+                  className="list-item opacity-0f"
                 >
-                  <div className="flex flex-col">
-                    <span className="text-3xl font-bold">
-                      {item.data.title}
-                    </span>
-                    <div className="flex gap-3 text-yellow-400 text-lg font-bold">
-                      {item.tags.map((tag, index) => (
-                        <span key={index}>{tag}</span>
-                      ))}
+                  <Link
+                    href={urlPrefixes + "/" + item.uid}
+                    className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+                    aria-label={item.data.title}
+                  >
+                    <div className="flex flex-col">
+                      <span className="text-3xl font-bold">
+                        {item.data.title}
+                      </span>
+                      <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                        {item.tags.map((tag, index) => (
+                          <span key={index}>{tag}</span>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                  <span className="ml-auto flex items-center gap-2 font-medium text-xl md:ml-0">
-                    {viewMoreText} <MdArrowOutward />
-                  </span>
-                </Link>
-              </li>
-            )}
-          </>
-        ))}
+                    <span className="ml-auto flex items-center gap-2 font-medium text-xl md:ml-0">
+                      {viewMoreText} <MdArrowOutward />
+                    </span>
+                  </Link>
+                </li>
+              )}
+            </>
+          )
+        )}
       </ul>
 
       {/* Hover Element */}
