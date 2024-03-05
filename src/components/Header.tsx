57141d1237179ae1eadcfb00c6030cf3c25533e2
amine alaoui <omari1amineco@gmail.com>
2024-03-05 10:40:25 +0100

    feat: import NavBar component to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { createClient } from "@/prismicio";
-import { PrismicNextLink } from "@prismicio/next";
-import Link from "next/link";
 import React from "react";
+import NavBar from "@/components/Navbar";
 
 export default async function Header() {
   const client = createClient();
@@ -9,20 +8,7 @@ export default async function Header() {
 
   return (
     <header className="top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/" aria-label="Home page">
-              {settings.data.name}
-            </Link>
-          </li>
-          {settings.data.nav_item.map(({ link, label }, index) => (
-            <li key={index}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <NavBar />
     </header>
   );
 }
